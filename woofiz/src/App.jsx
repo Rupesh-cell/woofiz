09b3eca20e2c7c0e9704462b5474c6494b9d0ebc
Rupesh Khadka <rupesh@alwazebrand.com>
2022-11-26 15:55:23 +0530

    General patch

---
+++ woofiz/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+
+
+const App = () => {
+	return (
+		<>
+			<Router>
+				
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/home" element={<Home />} />
+				</Routes>
+			</Router>
+		
+		</>
+	);
+};
+
+export default App;
---
+++ woofiz/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className="container">
+        jfbhndm,</div></>
+  )
+}
+
+export default Footer
