2aa1044267d399dd35348e950f081d67d5e7fba7
Rupesh Khadka <78486743+Rupesh-cell@users.noreply.github.com>
2022-12-14 13:36:20 +0530

    image

---
+++ woofiz/Frontend/src/Components/Featured.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Featured = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Featured
--- woofiz/Frontend/src/Components/data/image.js
+++ woofiz/Frontend/src/Components/data/image.js
@@ -1,5 +1,11 @@
-const Image [
-    { 
-        
-    }
-]
+export const Image = [
+  {
+    name: "Bad",
+    src: "https://static.toiimg.com/photo/msid-83566892/83566892.jpg",
+
+  },
+  {
+    name:"no",
+    src:"https://static.toiimg.com/photo/msid-83566892/83566892.jpg"
+  }
+];
