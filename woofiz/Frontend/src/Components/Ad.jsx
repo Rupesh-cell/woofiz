26193ff8e4aa6051e9754a1c5afe86fbfdfba843
Rupesh Khadka <78486743+Rupesh-cell@users.noreply.github.com>
2022-12-21 13:23:48 +0530

    api

--- woofiz/Frontend/src/Components/Ad.jsx
+++ woofiz/Frontend/src/Components/Ad.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
-import '../Scss/ad.scss';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../Scss/ad.scss";
+
 
 const Ad = () => {
+  const [banner, setBanner] = useState([]);
+  console.log(banner)
+
+
+  const setData = () => {
+    const options = {
+      method: "GET",
+      url: "http://localhost:3001/api/salesbanner",
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setBanner(response.data.banner);
+
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    setData()
+  }, []);
+
   return (
     <>
-    <div className="image-container">
-        <div className="image">
-            <img src="http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcSQX2h2LlLZuvhnfp_02JYoLXvWvJROn-5xs70BE5RLLSiwww0qAsDVF0wKS_2Wq3nAlwDjbSMcBRo3DZDoxjo" alt="" />
-        </div>
-    </div>
+      <div className="image-container">
+        {banner?(banner?.map((items,index ) => {
+          return ( <div className="image">
+          <img
+          src={items.image}
+            alt=""
+          />
+        </div>)
+
+        })  ) : (<h1>No Data available</h1>)}
+        
+      </div>
     </>
-  )
-}
+  );
+};
 export default Ad;
--- woofiz/Frontend/src/Components/Carousel.jsx
+++ woofiz/Frontend/src/Components/Carousel.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {Carousel} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Scss/Carousel.scss';
 
 const Carouse = () =>{
+  
   return (
     <>
     <Carousel>
