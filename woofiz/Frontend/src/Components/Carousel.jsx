24019729004e412ae5dce48f1790e579b4188a07
Rupesh Khadka <78486743+Rupesh-cell@users.noreply.github.com>
2022-12-22 16:43:33 +0530

    carousel api
    added carousel api

--- woofiz/Frontend/src/Components/Carousel.jsx
+++ woofiz/Frontend/src/Components/Carousel.jsx
@@ -1,51 +1,56 @@
-import React,{useState,useEffect} from "react";
-import {Carousel} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../Scss/Carousel.scss';
+import React, { useState, useEffect } from "react";
+import { Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../Scss/Carousel.scss";
+import axios from "axios";
 
-const Carouse = () =>{
+const Carouse = () => {
+  const [carousel, setCarousel] = useState([]);
+  console.log(carousel)
   
+  const getCarousel = () => {
+    const option = {
+      method: "GET",
+      url: "http://localhost:3001/api/carousel/all",
+    };
+
+    axios
+      .request(option)
+      .then(function (response) {
+        setCarousel(response.data.carousels);
+      })
+
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getCarousel();
+  }, []);
+
   return (
     <>
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="https://scontent.fpnq9-1.fna.fbcdn.net/v/t1.6435-9/186484352_738217190185816_210249930828436604_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WCP3dxyAzZ8AX_igi1r&_nc_ht=scontent.fpnq9-1.fna&oh=00_AfDVJhTdr-_xhekGAgGYFjjYw806uo_ztcHHuG7yGYbhyw&oe=63B86B4A"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"A
-          src="https://scontent.fpnq9-1.fna.fbcdn.net/v/t1.6435-9/186484352_738217190185816_210249930828436604_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WCP3dxyAzZ8AX_igi1r&_nc_ht=scontent.fpnq9-1.fna&oh=00_AfDVJhTdr-_xhekGAgGYFjjYw806uo_ztcHHuG7yGYbhyw&oe=63B86B4A"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://scontent.fpnq9-1.fna.fbcdn.net/v/t1.6435-9/186484352_738217190185816_210249930828436604_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WCP3dxyAzZ8AX_igi1r&_nc_ht=scontent.fpnq9-1.fna&oh=00_AfDVJhTdr-_xhekGAgGYFjjYw806uo_ztcHHuG7yGYbhyw&oe=63B86B4A"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+      <Carousel>
+        {carousel?carousel?.map((items,index ) => (
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src={items.image}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        )):<h1>no data</h1>
+        }
+        
+      </Carousel>
     </>
   );
-}
+};
 
 export default Carouse;
